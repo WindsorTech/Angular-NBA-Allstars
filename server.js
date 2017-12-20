8b5b87326916e54bac2b3c06a13087aaca6a5b29
windsor80 <windsorjunior@hotmail.com>
2017-12-19 20:13:40 -0500

    updates

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ var app = express();
 app.use(express.static(__dirname + "/app"));
 
 // Body Parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/*+json' }));
-app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
-app.use(bodyParser.text({ type: 'text/html' }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.json({ type: 'application/*+json' }));
+// app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+// app.use(bodyParser.text({ type: 'text/html' }));
 
 // Require Database Routes Files
 //var getRoute = require("./api/get-tasks.js")(app);
