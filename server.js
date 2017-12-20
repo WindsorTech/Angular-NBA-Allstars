d01e0b5fdcff8128e0939a22fb91fdede8bf6c62
windsor80 <windsorjunior@hotmail.com>
2017-12-19 20:07:11 -0500

    updates

---
+++ server.js
@@ -0,0 +1,30 @@
+// Dependencies
+var express = require('express');
+//var bodyParser = require('body-parser');
+var app = express();
+
+// Require Database Files
+//var db = require('./db/database-connect.js');
+//var Todo = require('./db/task-database.js');
+
+// Direct Express to the Angular App
+app.use(express.static(__dirname + "/app"));
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
+
+// Require Database Routes Files
+//var getRoute = require("./api/get-tasks.js")(app);
+//var postRoute = require("./api/post-tasks.js")(app);
+//var putRoute = require("./api/update-tasks.js")(app);
+//var deleteRoute = require("./api/delete-tasks.js")(app);
+
+//Server Port
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("App Up & Running! Port:", port);
+}); 
