e7ce890811c6f605129ffa2ab0f9231a4ed38581
windsor80 <windsorjunior@hotmail.com>
2018-01-24 19:14:25 -0500

    updates

--- app/nbaAllstars/features/components/nbaPlayer/nbaPlayer.directive.js
+++ app/nbaAllstars/features/components/nbaPlayer/nbaPlayer.directive.js
@@ -17,6 +17,8 @@ function NbaPlayer() {
 
 function NbaPlayerCtrl($scope) {
 	var vm = this;
+
+	$(".fb-share-button").hide();
 	
 
 }
--- app/nbaAllstars/features/game/game.controller.js
+++ app/nbaAllstars/features/game/game.controller.js
@@ -783,6 +783,12 @@ function GameCtrl() {
 
     });
 
+    // if ( $('#guard-1').hasClass('full') && $('#guard-2').hasClass('full') && $('#forward-1').hasClass('full') && $('#forward-2').hasClass('full') && $('#center-1').hasClass('full') ) {
+
+    //     $(".fb-share-button").show();
+
+    // }
+
 
 
 }
