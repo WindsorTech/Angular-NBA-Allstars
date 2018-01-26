755711c61d82d96eedf8c084896d1d79c40af556
windsor80 <windsorjunior@hotmail.com>
2018-01-26 17:48:29 -0500

    updates

--- app/nbaAllstars/features/components/nbaPlayer/nbaPlayer.directive.js
+++ app/nbaAllstars/features/components/nbaPlayer/nbaPlayer.directive.js
@@ -19,6 +19,21 @@ function NbaPlayerCtrl($scope) {
 	var vm = this;
 
 	$(".fb-share-button").hide();
+
+	$(".btn").click(function(){
+
+		if ( $('#guard-1').hasClass('full') && $('#guard-2').hasClass('full') && $('#forward-1').hasClass('full') && $('#forward-2').hasClass('full') && $('#center-1').hasClass('full') ) {
+
+			$(".fb-share-button").show();
+
+		} else {
+
+			$(".fb-share-button").hide();
+
+		}
+
+
+	});
 	
 
 }
